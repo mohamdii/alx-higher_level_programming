2d84226dc22d0fb0ef957c2912202732de57085a
mohamdii <m.hamdy97@gmail.com>
2024-01-02 19:10:04 +0000

    fixing indexes

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 str = "Holberton"
 word_first_3 = str[:3]
-word_last_2 = str[-3:]
+word_last_2 = str[-2:]
 middle_word = str[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
