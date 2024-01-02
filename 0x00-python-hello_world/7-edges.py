c10e7175bf88863f010328d398a7821743c42135
mohamdii <m.hamdy97@gmail.com>
2024-01-02 18:54:55 +0000

    adding the required string

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-str = "Holberton School"
+str = "Holberton"
 word_first_3 = str[:3]
 word_last_2 = str[-3:]
-middle_word = str[1:8]
-print(word_first_3)
-print(word_last_2)
-print(middle_word)
+middle_word = str[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
