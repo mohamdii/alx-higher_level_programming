085c09f096b51c5d811f264f3f502bfe117de73d
mohamdii <m.hamdy97@gmail.com>
2024-01-03 17:52:08 +0000

    fixing index

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(1, len(str)):
+    for i in range(0, len(str)):
         x = ord(str[i])
         if x >= 97 and x <= 122:
             x = x - 32
