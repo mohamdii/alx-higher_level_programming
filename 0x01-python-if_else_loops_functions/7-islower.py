12da48ac70f4ff6e833fac8eee6c853b614de064
mohamdii <m.hamdy97@gmail.com>
2024-01-03 13:54:24 +0000

    converting to ascii

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 97 and c <= 122:
+    if ord(c) >= 97 and ord(c) <= 122:
         print("{} is lower".format(c))
     else:
         print("{} is upper".format(c))
