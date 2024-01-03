6a0a3f036007076ab09cbd5101164fa20356dd0d
mohamdii <m.hamdy97@gmail.com>
2024-01-03 09:22:01 +0000

    fixing the output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str1 = "and is greater than 5"
+str1 = "and is greater than 5 and not 0"
 str2 = "is 0"
 str3 = "and is less than 6 and not 0"
 if number < 0:
