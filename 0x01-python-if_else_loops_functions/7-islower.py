3b8a4a672844f89b2dc5adb1a5a029fc36904d3e
mohamdii <m.hamdy97@gmail.com>
2024-01-03 13:57:39 +0000

    returning instead of printing

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     if c >= 97 and c <= 122:
-        print("{} is lower".format(c))
+        return True
     else:
-        print("{} is upper".format(c))
+        return False
