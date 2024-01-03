d4f3fe96042f458e221a55f6a653ee220245ecd2
mohamdii <m.hamdy97@gmail.com>
2024-01-03 09:34:12 +0000

    fixing string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 str1 = "and is greater than 5"
-str2 = "is 0"
+str2 = "and is 0"
 str3 = "and is less than 6 and not 0"
 if number < 0:
     lastdigit = number % -10
