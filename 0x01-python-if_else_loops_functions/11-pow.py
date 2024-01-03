7b12874f0199ea514adc4235569e48e2c30ebeb4
mohamdii <m.hamdy97@gmail.com>
2024-01-03 18:07:08 +0000

    1 git commit -m 1

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
     c = a
-    for i in range(b - 1):
-        a = a * c
+    if b > 0:
+        for i in range(b - 1):
+            a = a * c
+    else:
+        for i in range(b - 1):
+            a = 1 / a * 1 / c
     return a
