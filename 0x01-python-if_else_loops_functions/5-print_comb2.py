97d8d1dae7b45127faaa35d4f2a4f99e3af1b45b
mohamdii <m.hamdy97@gmail.com>
2024-01-03 10:16:50 +0000

    adding a new line

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(0, 10):
-        print("{}{}", end=", ")
+        if i == 9 and j == 9:
+            print("{}{}".format(j, j))
+            break
+        print("{}{}".format(i, j), end=", ")
