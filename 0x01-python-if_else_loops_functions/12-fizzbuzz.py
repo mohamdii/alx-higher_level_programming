e12860dac1a92b6e569a729b52221f54ce93fc72
mohamdii <m.hamdy97@gmail.com>
2024-01-04 00:22:45 +0000

    function added

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 5 == 0 and i % 3 == 0:
-        print("FizzBuzz", end=" ")
-        continue
-    elif i % 3 == 0:
-        print("Fizz", end=" ")
-        continue
-    elif i % 5 == 0:
-        if i == 100:
-            print("Buzz ")
-        print("Buzz", end=" ")
-        continue
-    print(f"{i}", end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz", end=" ")
+            continue
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+            continue
+        elif i % 5 == 0:
+            if i == 100:
+                print("Buzz ")
+            print("Buzz", end=" ")
+            continue
+        print(f"{i}", end=" ")
+fizzbuzz()
