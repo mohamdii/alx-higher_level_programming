65a9e5e05facd0c04eeabc0892d95e5a09907e53
mohamdii <m.hamdy97@gmail.com>
2024-01-05 01:00:30 +0000

    trying to understand how it works

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-import add_0
-print("{1 + 2}".format(add_0.add(1, 2)))
+if __name__ == "__main__":
+    from add_0 import add 
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
