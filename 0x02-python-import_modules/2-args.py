8c3377f6ab992b2c157d8e69b8b748134100f8ea
mohamdii <m.hamdy97@gmail.com>
2024-01-05 02:03:37 +0000

    it works

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    for i in range(len(argv)):
-        print("{}: {}".format(i,sys.argv[i + 1]))
+    for i in range(len(sys.argv)):
+        if i == 0:
+            continue
+        print("{}: {}".format(i,sys.argv[i]))
