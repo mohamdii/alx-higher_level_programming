71d25449ffa50c60c2f1738caaf3612eff2d3a3f
mohamdii <m.hamdy97@gmail.com>
2024-01-05 02:06:47 +0000

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    print("{} arguments:".format(len(sys.argv)))
     for i in range(len(sys.argv)):
         if i == 0:
             continue
