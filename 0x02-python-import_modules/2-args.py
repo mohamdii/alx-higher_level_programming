5ce810028ec9fcfd5c17e127486d87663f9ec941
mohamdii <m.hamdy97@gmail.com>
2024-01-05 02:10:06 +0000

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{} arguments:".format(len(sys.argv)))
+    if len(sys.argv) > 1:
+        print("{} arguments:".format(len(sys.argv)))
+    elif len(sys.argv) == 1
+        print("{} argument:".format(len(sys.argv)))
     for i in range(len(sys.argv)):
         if i == 0:
             continue
