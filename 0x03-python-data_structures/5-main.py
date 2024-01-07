f09c683199576c15e096f76b8863fb39dc03715f
mohamdii <m.hamdy97@gmail.com>
2024-01-07 21:20:33 +0200

    5

--- 0x03-python-data_structures/5-main.py
+++ 0x03-python-data_structures/5-main.py
@@ -2,5 +2,5 @@
 no_c = __import__('5-no_c').no_c
 
 print(no_c("Best School"))
-print(no_c("Chicago"))
+print(no_c("Chicagoc"))
 print(no_c("C is fun!"))
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
+    count = 0
     for i in my_string:
         if i == 'c' or i == 'C':
-            new_string = ''
+            new_string += ''
         else:
             new_string += i
     return new_string
