bda123000c5141acd6590d9cb71184376b5b855d
mohamdii <m.hamdy97@gmail.com>
2024-01-22 03:48:21 +0200

    3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,6 +6,7 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
+        return result
     finally:
         print("Inside result:{}".format(result))
         return result
