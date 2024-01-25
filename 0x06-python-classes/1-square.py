1057a216d7e28792e996c7e15a1bef0255ec9743
mohamdii <m.hamdy97@gmail.com>
2024-01-25 02:34:00 +0000

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class square"""
+
+class Square:
+    """size att."""
+
+    def __init__(self, size):
+        self.__size = size
+    def get(size):
+        return self.__size
+
