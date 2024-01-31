aea0f69a0231538101ca9a6408b519e6dfbc0402
mohamdii <m.hamdy97@gmail.com>
2024-01-31 23:31:31 +0000

    2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+
+
 class Rectangle:
     """Rectangle class"""
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -21,7 +23,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -33,7 +35,7 @@ class Rectangle:
 
     def area(self):
         return self.__height * self.__width
-    
+
     def perimeter(self):
         if (self.__height == 0 or self.__width == 0):
             return 0
