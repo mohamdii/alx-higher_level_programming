16a11d7690758f753199cf762c9dbee7731e12ce
mohamdii <m.hamdy97@gmail.com>
2024-02-03 00:00:29 +0200

    almost a rec

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
+""" a rectangle class"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
-class Rectangle(BaseGeometry):
 
+class Rectangle(BaseGeometry):
+    """ Almost a rectangle"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
