c746ade6bca3bc437e6f4a38b9d77bf8181efe5c
mohamdii <m.hamdy97@gmail.com>
2024-02-02 20:54:39 +0200

    1

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Compares between obj and class
+    Args:
+        obj (any type): input to check
+        a_class (targeted type): class
+    Returns:
+        if obj matches return True
+        otherwise return false
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
