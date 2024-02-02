ca5f8069f9cdd94210d88ddda4d77713b4137ee1
mohamdii <m.hamdy97@gmail.com>
2024-02-02 21:19:43 +0200

    isinstance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that compares if directly or inderectly inheriting from class"""
+
+def inherits_from(obj, a_class):
+    """
+    Check if the obj is directly or indirectly inheriting from class
+    Args:
+        obj (any type): input to be compared
+        a_class (target): class
+    Returns:
+        if obj is compared return True
+        otherwise - False
+    """
+    if (isinstance(bj, a_class)):
+        return True
+    return False
