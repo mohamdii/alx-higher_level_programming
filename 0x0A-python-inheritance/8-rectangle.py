713b61dc2c9253948da3b5c92bbd5cd3e265220e
mohamdii <m.hamdy97@gmail.com>
2024-02-02 23:34:53 +0200

    what is wrong

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(BaseGeometry):
     '''
     def __init__(self, width, height):
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
 
-        self.__width = width
         self.__height = height
