db72a8551bbba4f6de544daf4824ba58713796b0
mohamdii <m.hamdy97@gmail.com>
2024-02-02 23:32:59 +0200

    1

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Ract subclass"""
+'''
+    Implementing a Geometry class
+'''
+
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """rectanlge inherits for geometry !"""
+    '''
+        Implements a rectangle
+    '''
     def __init__(self, width, height):
-        self.integer_validator("width", height)
-        self.integer_validator("height", width)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
         self.__width = width
         self.__height = height
