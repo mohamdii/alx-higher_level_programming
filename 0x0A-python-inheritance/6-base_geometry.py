718f2d83b9d0da9c663e47f6591266155a39d804
mohamdii <m.hamdy97@gmail.com>
2024-02-02 21:38:14 +0200

    base geo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Geometry Class"""
+
+class BaseGeometry:
+    """Class that has only one public method"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
