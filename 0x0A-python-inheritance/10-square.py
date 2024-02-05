1161430bd9c0698478a1c0ab2527f85251e4f4c6
mohamdii <m.hamdy97@gmail.com>
2024-02-05 22:59:56 +0200

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Square Class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """initialize with sup() with size and size"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        Rectangle.__init__(self, width = size, height = size)
+        self.__size = size 
