1b567fc43b360f11abaa03dd5125ec6e66546da8
mohamdii <m.hamdy97@gmail.com>
2024-02-06 11:12:19 +0200

    adding comment

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+""" Read file function"""
 
 def read_file(filename=""):
+    """read file functon with handler"""
     with open(filename, mode = "r", encoding = "UTF8") as file:
         print(file.read())
