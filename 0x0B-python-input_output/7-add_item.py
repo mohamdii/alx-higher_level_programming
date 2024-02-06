9a90d18e980a4f09d55b1c3540f7454751ea6515
mohamdii <m.hamdy97@gmail.com>
2024-02-06 21:17:08 +0200

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,16 +2,14 @@
 """main function that adds args into alist """
 import sys
 
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
     lis = []
-    count = 0
-    for i in sys.argv:
-        if count == 0:
-            count+=1
-            continue
-        lis.append(i)
     filename = "add_item.json"
     save_to_json_file(lis, filename)
+    for i in sys.argv[1:]:
+        lis.append(i)
     load_from_json_file(filename)
