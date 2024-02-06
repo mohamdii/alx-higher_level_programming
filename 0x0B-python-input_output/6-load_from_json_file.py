a17c20b6f34e6ee5a6617792f7a6189700a3d8ba
mohamdii <m.hamdy97@gmail.com>
2024-02-06 15:02:31 +0200

    4

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """function return an object"""
     with open(filename, mode="r") as file:
-        json.dumps(file.read())
+        json.load(file)
