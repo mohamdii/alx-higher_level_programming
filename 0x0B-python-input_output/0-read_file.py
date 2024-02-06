0ac369444d262b194f66476eef674729937e0be6
mohamdii <m.hamdy97@gmail.com>
2024-02-06 11:19:14 +0200

    fixing code style

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Read file function"""
 
+
 def read_file(filename=""):
     """read file functon with handler"""
-    with open(filename, mode = "r", encoding = "UTF8") as file:
-        print(file.read())
+    with open(filename, mode="r", encoding="UTF8") as file:
+        print(file.read(), end="")
