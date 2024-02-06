be91087a6bb829f9e03cc1c07fbb6f87d35b5d0c
mohamdii <m.hamdy97@gmail.com>
2024-02-06 11:22:03 +0200

    fixing style

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ append function """
 
+
 def append_write(filename="", text=""):
     """ append_write adds to text"""
-    
+ 
     with open(filename, mode="a", encoding="utf-8") as file:
         return file.write(text)
