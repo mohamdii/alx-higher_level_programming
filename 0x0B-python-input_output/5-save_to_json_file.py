8a8f222d847f55fd59bb709663d055dc85b53f33
mohamdii <m.hamdy97@gmail.com>
2024-02-06 13:42:30 +0200

    5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """it modifies text file to take python object"""
     with open(filename, mode="w") as file:
-        file.write(json.loads(my_obj))
+        file.write(json.dumps(my_obj))
