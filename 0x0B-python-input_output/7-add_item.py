4ca777daaf6f6115f84f6fc3f64760edc1d0081c
mohamdii <m.hamdy97@gmail.com>
2024-02-06 21:11:10 +0200

    list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""main function that adds args into alist """
 import sys
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-l = []
-count = 0
-for i in sys.argv:
-    if count == 0:
-        count+=1
-        continue
-    l.append(i)
-filename = "add_item.json"
-"""load_json = load_from_json_file(filename)"""
-save_to_json_file(l, filename)
-
-load_from_json_file(filename)
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    lis = []
+    count = 0
+    for i in sys.argv:
+        if count == 0:
+            count+=1
+            continue
+        lis.append(i)
+    filename = "add_item.json"
+    save_to_json_file(lis, filename)
+    load_from_json_file(filename)
