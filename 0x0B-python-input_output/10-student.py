c78b62d4325f0a2e999f8db68f9072a8d8ec0005
mohamdii <m.hamdy97@gmail.com>
2024-02-07 03:58:15 +0200

    getattr

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Student class again"""
 
-class Student:
 
+class Student:
+    """student class with get attr"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
