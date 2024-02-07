ab7d00c73beda815b7c621ee2bbddb16d4a1932b
mohamdii <m.hamdy97@gmail.com>
2024-02-07 04:27:25 +0200

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+""" Student Class """
 
 
 class Student:
-
+    """student class """
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
