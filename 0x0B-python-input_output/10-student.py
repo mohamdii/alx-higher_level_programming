785bdd814a01c31797e71e982e4731cc1d91cc71
mohamdii <m.hamdy97@gmail.com>
2024-02-07 03:57:04 +0200

    10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Student class again"""
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, (list, str)):
+            return {attr: getattr(self, attr, None) for attr in attrs}
+        return self.__dict__
+    
