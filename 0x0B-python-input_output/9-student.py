450a9c7a7f0a72ed9c443cc1e6cd246093d0527b
mohamdii <m.hamdy97@gmail.com>
2024-02-07 03:31:50 +0200

    easier

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+""" Class student """
 
 
 class Student:
-    
+    """ returns a dict"""    
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
