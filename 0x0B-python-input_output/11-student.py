1ba7a61e2d0a30973464c46336840183d5ce4179
mohamdii <m.hamdy97@gmail.com>
2024-02-07 04:25:38 +0200

    it s wrong

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" Student class again"""
 
 
 class Student:
-    """student class with get attr"""
+
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
+        self.age =age
 
-    def to_json(self, attrs=None):
+    def to_json(self, attrs = None):
         if isinstance(attrs, (list, str)):
-            return {attr: getattr(self, attr, None) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
-    
+
+    def reload_from_json(self, json):
+        return self.to_json(json)
