04e280b47a4112d069925289f8d8159d4c135bb9
mohamdii <m.hamdy97@gmail.com>
2024-02-07 03:25:38 +0200

    ez pz

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """convert instance of class to json """
-import json
-
 
 def class_to_json(obj):
-    """process """
-    json.dumps(obj)
+    """ez use .__dict__"""
+    return obj.__dict__
