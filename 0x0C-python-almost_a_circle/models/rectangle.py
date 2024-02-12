2d9ce356f4b0451fe5e4d86bb464804e2d1f77fd
mohamdii <m.hamdy97@gmail.com>
2024-02-12 21:03:14 +0200

    s

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """ Rectangle from base"""
-
 from models.base import Base
 
 
 class Rectangle(Base):
-
     '''
         rectangle methods and const.
         Inherist from:
             Base
     '''
-    """ base class"""
 
     def __init__(self, width, height, x=0, y=0, id=None): 
         self.width = width
