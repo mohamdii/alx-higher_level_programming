93f8d0bf8e5f7268b81956b4dc83a65600788ff7
mohamdii <m.hamdy97@gmail.com>
2024-02-12 22:05:37 +0200

    1

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Define Square Class"""
+"""Define Rectangle Class
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ square class"""
+    """Module Representation of Square
+"""
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
         super().__init__(size, size, x, y, id)
