b13f0fe3a1c21625cbabcc658647f45a81e1c5e9
mohamdii <m.hamdy97@gmail.com>
2024-02-12 20:49:35 +0200

    bs

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ class Rectangle(Base):
     """ rectangle methods and const."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        
         self.width = width
         self.height = height
         self.x = x
