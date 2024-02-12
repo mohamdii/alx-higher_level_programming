b7c15ef18db9a13339133f913e82e0b86157ecba
mohamdii <m.hamdy97@gmail.com>
2024-02-12 21:45:41 +0200

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""square """
 import json
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-
+    """ square class"""
     def __init__(self, size, x=0, y=0, id=None):
         self.size = size
         super().__init__(size, size, x, y, id)
