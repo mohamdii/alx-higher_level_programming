553447153dd01285b6aaf3ce01911e05b7f0fa24
mohamdii <m.hamdy97@gmail.com>
2024-02-12 22:26:15 +0200

    s

---
+++ 0x0C-python-almost_a_circle/tests/rectangle_unitest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""testing func rectangle"""
+import unittest
+import rectangle
+
+
+class TestRectangle(unittest.TestCase):
+
+    def test_rectangle_output(self):
+        """testing the output of rect"""
+
+        self.assertAlmostEqual(rectangle(,)
+
+    def test_rectangle_input(self):
+        """testing input"""
+
+        self.assertRaises(TypeError, rectangle, )
