579c3a190bbcef14b87fdf5605b1b8d10da50360
mohamdii <m.hamdy97@gmail.com>
2024-02-12 20:46:18 +0200

    1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+""" Rectangle from base"""
 from models.base import Base
+
+
 class Rectangle(Base):
-    
+    """ rectangle methods and const."""
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
@@ -12,12 +15,15 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
+
     @property
     def height(self):
         return self.__height
+
     @property
     def x(self):
         return self.__x
+    
     @property
     def y(self):
         return self.__y
@@ -66,7 +72,8 @@ class Rectangle(Base):
             print()
 
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, \
+                self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         try:
