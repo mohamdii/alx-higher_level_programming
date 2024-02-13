c7b57f98a6fc9e0aab04d162bdcc5a85e2a7e8bc
mohamdii <m.hamdy97@gmail.com>
2024-02-13 10:39:10 +0200

    10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,31 +13,29 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initialiize a Square
         """
-        self.__size = size
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         '''getter for size
         '''
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         '''setter for size
         '''
         self.validate_setter("width", value)
-        self.__width = value
+        self.width = value
         self.validate_setter("height", value)
-        self.__height = value
-        self.__size = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         '''update function
         '''
         try:
             self.id = args[0]
-            self.__size = args[1]
+            self.__width = args[1]
             self.__x = args[2]
             self.__y = args[3]
         except IndexError:
@@ -50,4 +48,4 @@ class Square(Rectangle):
         '''str
         '''
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, \
-                                                 self.__size)
+                                                 self.width)
