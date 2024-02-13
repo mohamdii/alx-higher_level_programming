78c1929079dd17f312a75ce538138ee3ad31ba72
mohamdii <m.hamdy97@gmail.com>
2024-02-13 08:27:26 +0200

    models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -62,4 +62,4 @@ class Rectangle(Base):
             raise ValueError("{} must be > 0".format(name))
 
     def area(self):
-        return self.__width * self.__height
+        return (self.__width * self.__height)
