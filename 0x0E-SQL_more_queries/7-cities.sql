7f086526c2d0171847005af41b78ecff31333891
mohamdii <m.hamdy97@gmail.com>
2024-03-13 23:29:24 +0200

    name

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,9 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
-    id INT UNIQUE NOT NULL,
-    FOREIGN KEY (state_id) INT NOT NULL REFERENCES TO states(id),
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256),
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id),
     PRIMARY KEY (id)
 );
