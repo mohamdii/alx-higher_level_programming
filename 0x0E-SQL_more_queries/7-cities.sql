6d96787dd143d8167259fe4af527e29bf17caf8e
mohamdii <m.hamdy97@gmail.com>
2024-03-13 23:22:57 +0200

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- REFERENCE Key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT UNIQUE NOT NULL,
+    FOREIGN KEY (state_id) INT NOT NULL REFERENCES TO states(id),
+    PRIMARY KEY (id)
+);
