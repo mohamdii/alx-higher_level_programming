a5d8111409ce1320850623824ae563b28434e0e4
mohamdii <m.hamdy97@gmail.com>
2024-03-13 12:22:28 +0200

    2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- User 2
-CREATE DATABASE hbtn_0d_2 IF NOT EXISTS; 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2; 
 CREATE USER IF NOT EXISTS user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-TO user_0d_2
-ON hbtn_0d_2@localhost;
+ON hbtn_0d_2.*
+TO user_0d_2_pwd@localhost;
