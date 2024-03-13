4265a2e3cf1d964326d84a2007836f6e66a33ef3
mohamdii <m.hamdy97@gmail.com>
2024-03-13 23:32:11 +0200

    back to 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,4 @@
 -- create a new db and table name states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-	id INT UNIQUE NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
-	);
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
