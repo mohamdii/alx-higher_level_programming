f3180ed19a930a55ff7e8ba90b7fc13c38fdb2a3
mohamdii <m.hamdy97@gmail.com>
2024-04-08 14:43:54 +0200

    bs

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-const langs = ["C is fun", "Python is cool", "JavaScript is amazing"]
-for(i = 0; i < langs.length; i++)
-{
-    console.log(langs[i]);
-}
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+    console.log(line);
+  }
