81f6aa5604aae1d47290bf650b678322d1de2012
mohamdii <m.hamdy97@gmail.com>
2024-04-08 12:41:33 +0200

    2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,11 @@
 
 const argv = require('process').argv;
 
-argv.forEach((val, index) => {
-    if (index > 1 ) {
-        console.log('Arguments found');
-    } else {
-        console.log('No argument');
-    }
-});
+if (argv != null)
+{
+    console.log('Argument found');
+}
+else
+{
+    console.log('No argument');
+}
