1274c9904a56b6824fffcfe2697b4f15ab149ce2
mohamdii <m.hamdy97@gmail.com>
2024-04-08 15:10:11 +0200

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+argv = require('process').argv;
+
+let x, i, row;
+row = '';
+x = parseInt(argv[2]);
+if (isNaN(x))
+{
+    console.log('Missing number of occurrences')
+}
+for (i = 0; i < x; i++)
+{
+    for (let j = 0; j < x; j++)
+    {
+        row +='X'
+    }
+    if (i !== x)
+    {
+        row +='\n'
+    }
+}
+console.log(row)
