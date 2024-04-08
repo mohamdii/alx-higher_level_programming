a5431d5ba5c0599e811064b99adcbcbd98f5a5c1
mohamdii <m.hamdy97@gmail.com>
2024-04-08 14:29:57 +0200

    isnan

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const argv = require("process").argv;
+let x;
+x = parseInt(argv[2]);
+if (isNaN(x))
+{
+    console.log('Not a number')
+}
+else
+{
+    console.log('My number:' + x)
+}
