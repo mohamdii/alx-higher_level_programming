db6e4f4cf3bc364eba19aaf344d0c2d1ef405056
mohamdii <m.hamdy97@gmail.com>
2024-04-09 13:34:36 +0200

    s

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const Rectangle = require("./4-rectangle");
+
+class Square extends Rectangle {
+    constructor(size) {
+        super(size, size);
+        this.size = size;
+    }
+    charPrint(c) {
+        if (c == null)
+        {
+            this.print();
+        }
+        else {
+                let p = '';
+                for(let i = 0; i < this.size; i++) {
+                    for (let j = 0; j < this.size; j++) {
+                        p += c;
+                    }
+                    if (i < this.size - 1)
+                    {
+                        p += '\n'
+                    }
+                }
+                console.log(p)
+            }
+    }
+}
+module.exports = Square;
