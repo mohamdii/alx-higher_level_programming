e878a98f72ad9097627da83a84b307fa75cb5e52
mohamdii <m.hamdy97@gmail.com>
2024-04-09 12:56:02 +0200

    lol

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor(width, height){
-        if (width <= 0 || height <= 0 || !Number.isInteger(width) || !Number.isInteger(height))
-        {
-            this.width = undefined;
-            this.height = undefined;
-        }
-        else {
-            this.width = width;
-            this.height = height;
-        }
+    constructor(w, h){
+     if (w > 0 && h > 0)
+     {
+        this.width = w;
+        this.height = h;
+     }
     }
 }
+
 module.exports = Rectangle;
