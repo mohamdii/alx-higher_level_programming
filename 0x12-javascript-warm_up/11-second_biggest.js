87623e1361ef9d2fe9553d98a62b6c2b18fa12e5
mohamdii <m.hamdy97@gmail.com>
2024-04-09 15:48:57 +0200

    wrong and idk why

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,30 +1,32 @@
 #!/usr/bin/node
 
-const  argv  = require("process").argv;
+const { Console } = require("console");
 
-if (argv.length < 2)
+const  argv  = require("process").argv;
+if (argv.length <= 2)
 {
     console.log(0);
-}
-let dif = 0;
-let biggest = argv[2]
-let second = argv[2]
-for (let i = 2; i < argv.length; i++)
-{
-    if (biggest < argv[i])
+} else {
+    let biggest = argv[2]
+    let second = argv[2]
+    for (let i = 2; i < argv.length; i++)
     {
-        biggest = argv[i]
+        if (biggest < argv[i])
+        {
+            biggest = argv[i]
+        }
     }
-}
-for (i = 2; i < argv.length; i++)
-{
-    if (argv[i] == biggest)
+    for (i = 2; i < argv.length; i++)
     {
-        continue;
+        if (argv[i] == biggest)
+        {
+            continue;
+        }
+        if (second < argv[i])
+        {
+            second = argv[i]
+        }
     }
-    if (second < argv[i])
-    {
-        second = argv[i]
-    }
+    console.log(second)
+    console.log(argv.length)
 }
-console.log(second)
