0ca0efd2ad778d298242cbc877eda019b4a56086
mohamdii <m.hamdy97@gmail.com>
2024-04-13 03:35:00 +0200

    s

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Still learning bs"""
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+import sys
+if __name__ == '__main__':
+    conD = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(conD.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        if instance.id == 4:
+            continue
+        print(instance.id, instance.name, sep=': ')
