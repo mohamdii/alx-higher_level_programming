1a86662fa950dae07dc69367849b51910bcb0fdf
mohamdii <m.hamdy97@gmail.com>
2024-04-13 04:07:01 +0200

    not sql injected

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == sys.argv[4]).first()
-    
-    if instance:
-        print(instance.id)
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexError:
+        print("Not found")
